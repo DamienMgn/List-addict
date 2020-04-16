b8697558bf3afd858ee360001bf32d7f368d6a9c
damien <damien@macbook-pro-de-damien.home>
2020-04-17 00:00:22 +0200

    remove web routes

--- routes/web.php
+++ routes/web.php
@@ -20,15 +20,3 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
-
-Route::get('/categories', 'CategoriesController@showCategories')->name('show_categories');
-
-Route::get('/delete/{category}', 'CategoriesController@deleteCategory')->name('show_cards')->middleware('categories');
-
-Route::get('/cards/{category}', 'CardsController@showCards')->name('show_cards');
-
-Route::get('/delete/task/{task}/{card}/{category}', 'TasksController@deleteTask')->name('delete_task')->middleware('categories', 'cards', 'tasks');
-
-Route::get('/delete/card/{card}/{category}', 'CardsController@deleteCard')->name('delete_card')->middleware('categories', 'cards');
-
-Route::get('/update/task/{task}/{card}/{category}', 'TasksController@updateTask')->name('update_task');
