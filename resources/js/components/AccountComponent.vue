4d134cd2f391a3b3a680529eb4c0301fd6b9a859
damien <damien@macbook-pro-de-damien.home>
2020-05-22 18:40:20 +0200

    add form update mail/password

--- resources/js/components/AccountComponent.vue
+++ resources/js/components/AccountComponent.vue
@@ -1,6 +1,17 @@
 <template>
     <div class="account-container">
         <h1>ACCOUNT COMPONENT</h1>
+        <form>
+            <div class="form-group">
+                <label for="update-mail">Modification adresse email</label>
+                <input type="email" class="form-control" id="update-mail" aria-describedby="emailHelp">
+            </div>
+            <div class="form-group">
+                <label for="update-password">Modification mot de passe</label>
+                <input type="password" class="form-control" id="update-password">
+            </div>
+            <button type="submit" class="btn btn-primary">Modifier</button>
+        </form>
     </div>
 </template>
 
